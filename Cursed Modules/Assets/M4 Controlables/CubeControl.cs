37d16f65130db3bd76fdbd24f9b493562b22b098
Thulium Games <32959306+ThuliumGames@users.noreply.github.com>
2019-03-16 07:54:09 -0400

    Rideables

--- Cursed Modules/Assets/M4 Controlables/CubeControl.cs
+++ Cursed Modules/Assets/M4 Controlables/CubeControl.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CubeControl : Rideables {
 	
-	void Update () {
+	void LateUpdate () {
 		GetOn();
 		if (GetComponent<Rideables>().Riding) {
 			GetComponent<Rigidbody>().velocity = Camera.main.transform.right*SSInput.LHor[0] + Camera.main.transform.forward*SSInput.LVert[0];
+			if (SSInput.B[0] == "Pressed" && SSInput.LHor[0] == 0 && SSInput.LVert[0] == 0) {
+				GlobVars.Reading = false;
+				Interactables.StopI();
+			}
 		}
 	}
 }
