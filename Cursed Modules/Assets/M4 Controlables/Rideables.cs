2af15349b4da126a6012e5476b458cf97a1e3740
JamesGagnon1 <43007253+JamesGagnon1@users.noreply.github.com>
2019-04-12 14:12:47 -0400

    Rideables

---
+++ Cursed Modules/Assets/M4 Controlables/Rideables.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rideables : MonoBehaviour {
+	
+	public float[] Speeds;
+	public float TurnSpeed;
+	public Vector3 Offset;
+	public bool Riding;
+	public float PlayerMass;
+	GameObject Player;
+	
+	public void GetOn () {
+		Player = GameObject.Find("Player");
+		if (GlobVars.InteractObject == this.gameObject) {
+			Riding = true;
+			GlobVars.Reading = true;
+			Player.GetComponent<Movement>().enabled = false;
+			Player.GetComponent<Animator>().enabled = false;
+			Player.transform.position = transform.position + Offset;
+			Player.transform.rotation = transform.rotation;
+			Player.transform.parent = this.gameObject.transform;
+			PlayerMass = Player.GetComponent<Rigidbody>().mass;
+			Destroy(Player.GetComponent<Rigidbody>());
+
+		} else {
+			Riding = false;
+		}
+	}
+}
